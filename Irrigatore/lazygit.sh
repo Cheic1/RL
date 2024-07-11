44d2c7f6869e78d33dee099db59e238847421fe0
Cheic1 <54749359+Cheic1@users.noreply.github.com>
2024-07-11 13:41:47 +0000

    Fix del file lazy git con aggiutna di informazioni utili

--- Irrigatore/lazygit.sh
+++ Irrigatore/lazygit.sh
@@ -1,8 +1,11 @@
 #bash
-
-
+# Usare questi script per utilizzare pio
+# mkdir -p /usr/local/bin
+# sudo ln -s ~/.platformio/penv/bin/platformio /usr/local/bin/platformio
+# sduo ln -s ~/.platformio/penv/bin/pio /usr/local/bin/pio
+# sudo ln -s ~/.platformio/penv/bin/piodebuggdb /usr/local/bin/piodebuggdb
 # cd ..
-
+pio run --environment d1
 git add .
 git commit -a -m "$1"
 git push
